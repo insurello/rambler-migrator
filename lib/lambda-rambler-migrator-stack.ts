8dcd4e17cced1b1ccf3e50eea64852d50546e193
Rasmus Larsson <stoft@users.noreply.github.com>
2021-11-09 22:47:31 +0100

    initial lambda def etc.

--- lib/lambda-rambler-migrator-stack.ts
+++ lib/lambda-rambler-migrator-stack.ts
@@ -1,9 +1,49 @@
-import * as cdk from '@aws-cdk/core';
+import * as iam from "@aws-cdk/aws-iam";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as cdk from "@aws-cdk/core";
+import * as cr from "@aws-cdk/custom-resources";
+import * as path from "path";
 
 export class LambdaRamblerMigratorStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new lambda.DockerImageFunction(this, "func", {
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, "../")),
+    });
+
+    // ref: https://github.com/aws/aws-cdk/issues/10820
+    const lambdaTrigger = new cr.AwsCustomResource(this, "FunctionTrigger", {
+      policy: cr.AwsCustomResourcePolicy.fromStatements([
+        new iam.PolicyStatement({
+          actions: ["lambda:InvokeFunction"],
+          effect: iam.Effect.ALLOW,
+          resources: [fn.functionArn],
+        }),
+      ]),
+      // timeout: cdk.Duration.minutes(15),
+      onCreate: {
+        service: "Lambda",
+        action: "invoke",
+        parameters: {
+          FunctionName: fn.functionName,
+          InvocationType: "Event",
+        },
+        physicalResourceId: cr.PhysicalResourceId.of(
+          "JobSenderTriggerPhysicalId"
+        ),
+      },
+      onUpdate: {
+        service: "Lambda",
+        action: "invoke",
+        parameters: {
+          FunctionName: fn.functionName,
+          InvocationType: "Event",
+        },
+        physicalResourceId: cr.PhysicalResourceId.of(
+          "JobSenderTriggerPhysicalId"
+        ),
+      },
+    });
   }
 }
